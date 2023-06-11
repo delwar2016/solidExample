359b94b2d5abd49026ddbb2ddab9e13b8a190036
md delwar hossain <dhossain@testreach.com>
2023-06-11 19:43:35 +0600

    added partial factory pattern

---
+++ src/openClosePrinciple/factoryPattern/exam.ts
@@ -0,0 +1,75 @@
+class ExamResult {
+
+}
+
+class Question {
+
+}
+
+abstract class Exam {
+    id: string;
+    name: string;
+    duration: number;
+    questions: Question[];
+    protected constructor(id: string, name: string, duration: number, questions: Question[]) {
+        this.id = id;
+        this.name = name;
+        this.duration = duration;
+        this.questions = questions;
+    }
+
+    startExam(): void {
+        console.log('Exam is started');
+    }
+
+    submitExam(): ExamResult {
+        const examResult = new ExamResult();
+
+        // do calculation
+        console.log('Exam is submitted');
+        return examResult;
+    }
+}
+
+class MultipleChoiceExam extends Exam {
+    startExam() {
+        console.log('MultipleChoiceExam is started');
+    }
+
+    submitExam(): ExamResult {
+        const examResult = new ExamResult();
+        console.log('MultipleChoiceExam is submitted')
+        return examResult;
+    }
+}
+
+class EssayExam extends Exam {
+    startExam() {
+        console.log('EssayExam is started');
+    }
+
+    submitExam(): ExamResult {
+        const examResult = new ExamResult();
+        console.log('EssayExam is submitted')
+        return examResult;
+    }
+}
+
+class ExamFactory {
+    createExam(type: string, id: string, name: string, duration: number, questions: Question[]): Exam {
+       switch (type) {
+           case 'multiple-choice':
+               return new MultipleChoiceExam(id, name, duration, questions);
+       }
+    }
+
+    updateExam(id: string, name: string, duration: number) {
+        // do logic
+    }
+}
+
+
+const examService = new ExamService();
+const exam = examService.createExam('001', 'test 1', 30, [new Question()]);
+exam.startExam();
+exam.submitExam();
